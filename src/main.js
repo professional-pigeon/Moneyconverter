e99e2cb5101f79bc132cdcd2bb7a457088183a3a
professional-pigeon <kyle.kayperez@gmail.com>
2021-07-02 11:08:02 -0700

    create variable for any currency code

--- src/main.js
+++ src/main.js
@@ -5,15 +5,18 @@ import './css/styles.css';
 import CurrencyExchange from './serviceLogic.js'
 
 function conversion(response) {
-  let newValue = response.conversion_rates.${code} * dollar;
+  let dollar = $('#USD').val();
+  let code = $("#code").val();
+  let newValue = response.conversion_rates[code] * dollar;
+  console.log(dollar);
+  console.log(code);
+  console.log(newValue)
   $('#showHere').html("your " + dollar + " has been converted to " + newValue + " " + code + ".")
 }
 
 $(document).ready(function(){
   $('#form1').submit(function() {
-    let dollar = $('#USD').val();
-    let code = $("#code").val();
-    clearFields(); // create function
+    event.preventDefault();
     CurrencyExchange.getRates()
       .then(function(response) {
         conversion(response)
