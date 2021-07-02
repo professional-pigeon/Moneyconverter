9a25bfd0ccdd6a6235ea5445a1eb4d20411a5ce6
professional-pigeon <kyle.kayperez@gmail.com>
2021-07-02 09:30:03 -0700

    set up API with currency exchange website, exploring endpoints.

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import WeatherService from './weather-service.js'
+
+// body.conversion_rates.currencyCode
