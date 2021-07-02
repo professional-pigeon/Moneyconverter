712896a4deabd2fc7a16dce98772853847a941ba
professional-pigeon <kyle.kayperez@gmail.com>
2021-07-02 10:17:03 -0700

    create UI logic for conversion and printing conversion to DOM

--- src/main.js
+++ src/main.js
@@ -3,5 +3,24 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyExchange from './serviceLogic.js'
+import { ConcatenationScope } from 'webpack';
 
 // body.conversion_rates.currencyCode
+
+function conversion(response) {
+  let newValue = response.conversion_rates.${code} * dollar;
+  $('#showHere').html("your " + dollar + " has been converted to " + newValue + " " + code + ".")
+}
+
+$(document).ready(function(){
+  $('#form1').submit(function() {
+    let dollar = $('#USD').val();
+    let code = $("#code").val();
+    clearFields(); // create function
+    CurrencyExchange.getRates()
+      .then(function(response) {
+        conversion(response)
+      })
+
+  })
+})
